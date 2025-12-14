a5fb0ac90d5dfc92777af0b637d429720301fde8
Wearily <parsapournabi1382@gmail.com>
2025-12-14 22:59:03 +0330

    add module template from LibraryTemplate repo

---
+++ examples/main.cpp
@@ -0,0 +1,11 @@
+#include <QCoreApplication>
+#include <iostream>
+
+// #include <WeaCore/ModuleTest.h>
+
+int main(int argc, char** argv)
+{
+    QCoreApplication app(argc, argv);
+    // std::cout << "Example for " << "@MODULE_NAME@" << std::endl;
+    return 0;
+}
