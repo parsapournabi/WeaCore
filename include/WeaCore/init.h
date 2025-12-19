2daf27a61cb759c60f5e2f8a8da223a198e3225e
Wearily <parsapournabi1382@gmail.com>
2025-12-19 18:41:33 +0330

    add CMake macros defintion & init.h
    added: namespace macros into the init.h.

---
+++ include/WeaCore/init.h
@@ -0,0 +1,17 @@
+#ifndef INIT_H
+#define INIT_H
+
+/*!
+ * \brief WEACORE_NAMESPACE_START: module namespace start
+ */
+#define WEACORE_NAMESPACE_START namespace MODULE_NAME {
+
+/*!
+ * \brief WEACORE_NAMESPACE_END : module namespace end
+ */
+#define WEACORE_NAMESPACE_END };
+
+
+#define USE_NAMESPACE_WEACORE using namespace MODULE_NAME;
+
+#endif // INIT_H
