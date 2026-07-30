09c0bcea8b676425d9cf6312c64ae03d80150a4c
Wearily <parsapournabi1382@gmail.com>
2026-07-30 23:25:02 +0330

    example: add simple example for LoggerStream class

--- examples/MainClass.h
+++ examples/MainClass.h
@@ -1,13 +1,42 @@
 #ifndef MAINCLASS_H
 #define MAINCLASS_H
 
-#include "WeaCore/utils.h"
+#include "WeaCore/loggerstream.h"
+#include "WeaCore/datetime.h"
+
+// USE_NAMESPACE_WEACORE
+enum Test
+{
+    First = 0,
+    Second
+};
 
 class MainClass : public QObject
 {
 
         Q_OBJECT
         W_PROP_HDEF(QString, prop1, Prop1, "Property1")
+    public:
+        MainClass()
+        {
+            WeaCore::LoggerStream::setLogLevelShort(true);
+            WeaCore::LoggerStream::setDefaultDateTime({WeaCore::DateTime::HMSZ, WeaCore::DateTime::CompleteName});
+
+            QString value = "Parsa POurnabi";
+            wDebug() << value;
+            wDebug().nologlevel() << value;
+            wWarning().notimestamp() << value;
+            wInfo().noclassname() << value;
+            wCritical().nofunctionname() << value;
+            wError().noline().line() << value;
+            wDebug().datetimeFormat(WeaCore::DateTime::DateTimeFormat::All) << value;
+            wDebug().datetimeMonthFormat(WeaCore::DateTime::MonthFormat::Number) << value;
+            wDebug().datetimeMonthFormat(WeaCore::DateTime::MonthFormat::CompleteName) << value;
+            // wDebug().noline() << value;
+            Test t = First;
+            qDebug() << Test::First;
+        }
+        Q_ENUM(Test)
 };
 
 
